da87976012c9d355a3e0096957f43d8e1c7d169a
Thomas Dost <stu203329@mail.uni-kiel.de>
2024-05-02 11:52:26 +0200

    Added save dialog

--- src/main.js
+++ src/main.js
@@ -1,24 +1,52 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 const path = require('path');
+const fs = require('fs');
+
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            preload: path.join(__dirname, 'preload.js'),
+  const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+          nodeIntegration: true,
+          preload: path.join(__dirname, 'preload.js'),
+      }
+  });
+
+  const openSaveDialog = (data) => {
+    dialog.showSaveDialog(win, {
+      title: 'Save File', // Title of the dialog
+      defaultPath: path.join(app.getPath('documents'), 'untitled.json'), // Default path and filename
+      buttonLabel: 'Save', // Label for the save button
+      filters: [
+        { name: 'Petri Nets', extensions: ['json'] }, // Limit file types to .txt
+        { name: 'All Files', extensions: ['*'] } // Allow all file types
+      ]
+    }).then(result => {
+      if (!result.canceled) {
+        console.log('Selected file path:', result.filePath);
+        try {
+          fs.writeFileSync(result.filePath, data)
+        } catch (err) {
+          console.error(err)
         }
+      }
+    }).catch(err => {
+      console.error(err);
     });
-
-    ipcMain.handle('ping', () => 'pong')
-    ipcMain.handle('save', (event, data) => {
-      console.log(data)
-    })
+  };
 
 
-    win.webContents.openDevTools();
-    win.loadFile('index.html');
+
+  ipcMain.handle('ping', () => 'pong')
+  ipcMain.handle('save', (event, data) => {
+    openSaveDialog(JSON.stringify(data))
+    console.log(data)
+  })
+
+
+  win.webContents.openDevTools();
+  win.loadFile('index.html');
 }
 
 app.whenReady().then(createWindow);
